ad858796a19fd4bbf3493ad899f82e11a918c03d
Usmanfawad <45047731+Usmanfawad@users.noreply.github.com>
2018-11-28 20:21:45 +0500

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -1,13 +1,13 @@
 print(u"\u25A0"*28)
 print("Time to play a guessing game")
 import time
-print(u"\u25A0"*20)
+print(u"\u25A0"*5)
 time.sleep(2)
-print(u"\u25A0"*18)
+print(u"\u25A0"*10)
 time.sleep(2)
 print(u"\u25A0"*15)
 time.sleep(2)
-print(u"\u29A0"*10)
+print(u"\u29A0"*20)
 import random
 x= random.randint(1,100)
 guess= int(input("guess a number from 1 to 100: "))
